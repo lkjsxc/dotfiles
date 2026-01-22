c8f9a117e7c606530e52786a92bf4e8d36de6151
lkjsxc <lkjsxc@gmail.com>
2026-01-22 14:19:21 +0900

    chore(flake): import local overlay for prism-launcher

--- flake.nix
+++ flake.nix
@@ -1,7 +1,12 @@
 {
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-  outputs = { self, nixpkgs }: {
-    nixosConfigurations.main = nixpkgs.lib.nixosSystem {
+  outputs = { self, nixpkgs, ... }: let
+    system = "x86_64-linux";
+    overlays = [ (import ./overlays/prism.nix) ];
+    pkgs = import nixpkgs { inherit system overlays; };
+  in {
+    nixosConfigurations.main = pkgs.lib.nixosSystem {
+      system = system;
       modules = [ ./configuration.nix ./hardware-configuration.nix ];
     };
   };
