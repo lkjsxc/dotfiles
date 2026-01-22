70625165a02fcdae4a9925a85a2b1c4ddb7a559a
lkjsxc <lkjsxc@gmail.com>
2026-01-22 11:47:17 +0900

    feat(flakes): add docs build target and devShell

--- flake.nix
+++ flake.nix
@@ -1,8 +1,32 @@
 {
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-  outputs = { self, nixpkgs }: {
+
+  outputs = { self, nixpkgs, ... }: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
     nixosConfigurations.main = nixpkgs.lib.nixosSystem {
       modules = [ ./configuration.nix ./hardware-configuration.nix ];
+    };
+
+    # A tiny Nix package that builds HTML docs from Markdown using pandoc.
+    packages.${system}.docs = pkgs.runCommand "dotfiles-docs" {
+      buildInputs = [ pkgs.pandoc ];
+    } ''
+      mkdir -p $out
+      for f in docs/*.md; do
+        base=$(basename "$f" .md)
+        pandoc -s -o $out/$base.html "$f"
+      done
+      cp -r docs $out/docs
+    '';
+
+    # Dev shell to make it convenient to build docs locally.
+    devShells.${system}.default = pkgs.mkShell {
+      buildInputs = [ pkgs.pandoc ];
+      shellHook = ''
+        echo "Dev shell: pandoc available to build docs (run scripts/build_docs.sh)"
+      '';
     };
   };
 }
