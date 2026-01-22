ddd22a0387b46505cc668de204af0f180b6adb51
lkjsxc <lkjsxc@gmail.com>
2026-01-22 11:04:18 +0900

    .

--- flake.nix
+++ flake.nix
@@ -1,7 +1,7 @@
 {
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   outputs = { self, nixpkgs }: {
-    nixosConfigurations.myNixOS = nixpkgs.lib.nixosSystem {
+    nixosConfigurations.main = nixpkgs.lib.nixosSystem {
       modules = [ ./configuration.nix ./hardware-configuration.nix ];
     };
   };
