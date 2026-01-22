d6b20be0e6f7e0c13571fa55f9ccc5f7b462100b
lkjsxc <lkjsxc@gmail.com>
2026-01-22 14:02:17 +0900

    docs: point configuration.nix to docs/CONFIGURATION.md

--- configuration.nix
+++ configuration.nix
@@ -1,6 +1,8 @@
 # Edit this configuration file to define what should be installed on
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
+#
+# Documentation: detailed editing and examples are in ./docs/CONFIGURATION.md
 
 { config, pkgs, ... }:
 
